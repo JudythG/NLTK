4f4fa680c353371f7349c51594a1258ba9f0ba7a
JudythG <47835823+JudythG@users.noreply.github.com>
2019-04-28 12:14:38 -0400

    Add files via upload

--- token_frequency/freq_dist.py
+++ token_frequency/freq_dist.py
@@ -1,25 +1,36 @@
-# Retreive data from STANDS4 Quotes API
+# Get Frequency Distribution of 50 most common words with a length greater
+# than three characters  in Walt Whitman's 'I Sing the Body Electric'
 
 import requests
-import nltk
 from nltk.probability import FreqDist
 from nltk.tokenize import word_tokenize
+import io
+import csv
 
 def main():
-    req = requests.get ("http://poetrydb.org/title/Tiresias/lines.json")
-    #req = requests.get ("http://poetrydb.org/title/Defrauded I a Butterfly --/lines.json")
-    if req.status_code == 200:
-        poems = req.json()
+    # get poem text from Poetry DB
+    req = requests.get ('http://poetrydb.org/title/I%20Sing%20the%20Body%20Electric/lines,author,title')
+    if req.status_code == 200:	# if success
+        poems = req.json()	# convert to JSON
 
         for poem in poems:
 
+            # accumulate lines of poem into lines
             lines = ''
             for line in poem['lines']:
                 lines += ' ' + line
-            poem_text = nltk.word_tokenize(lines)
 
-            fdist = FreqDist(word.lower() for word in word_tokenize(lines))
-            print (fdist.most_common(25))
+            # calculate frquency distribution for words of length > 3
+            fdist = FreqDist(word.lower() for word in word_tokenize(lines) if len(word) > 3)
+
+            # ouput to CSV file
+            with open ('token_count_LeavesOfGrass.csv', 'w', newline='') as csvfile:
+                fieldnames = ['Word', 'Frequency']
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                writer.writeheader()
+                for word, count in fdist.most_common(50):
+                    writer.writerow({'Word': word, 'Frequency': count})
+
 
 # main
 main ()
